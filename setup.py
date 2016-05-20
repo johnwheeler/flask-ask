1e1b8ba71dd68d2522b2cceaefd6778cb400101c
jwheeler79 <jwheeler1@gmail.com>
2016-05-20 09:45:58 -0700

    version bump

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ from setuptools import setup
 
 setup(
     name='Flask-Ask',
-    version='0.1',
+    version='0.2',
     url='https://github.com/johnwheeler/flask-ask',
     license='BSD',
     author='John Wheeler',
