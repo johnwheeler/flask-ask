0a475354a6d3fde7c7ad91626b3a879b6c50392a
johnwheeler <john@johnwheeler.org>
2016-08-02 21:08:15 -0700

    bona fide state machine demo (wip)

--- samples/flight_reservation/flight_reservation.py
+++ samples/flight_reservation/flight_reservation.py
@@ -11,43 +11,43 @@ logging.getLogger("flask_ask").setLevel(logging.DEBUG)
 
 @ask.launch
 def welcome():
-    state.transition = 'from_city'
+    state.transition('from_city')
     return question("Welcome to the flight booking service. What city are you leaving from?")
 
 
 @ask.intent("CityIntent", state='from_city')
 def from_city(city):
-    state.transition = 'to_city'
+    state.transition('to_city')
     return question("Where are you going?")
 
 
 @ask.intent("CityIntent", state='to_city')
 def to_city(city):
-    state.transition = 'from_date'
+    state.transition('from_date')
     return question("What date do you want to leave?")
 
 
 @ask.intent("DateIntent", state='from_date')
 def from_date(date):
-    state.transition = 'one_way'
+    state.transition('one_way')
     return question("Is it a one-way trip?")
 
 
 @ask.intent("AMAZON.YesIntent", state='one_way')
 def one_way_yes():
-    state.transition = 'confirm'
+    state.transition('confirm')
     return question("Do you want to confirm this trip?")
 
 
 @ask.intent("AMAZON.NoIntent", state='one_way')
 def one_way_no():
-    state.transition = 'from_date'
+    state.transition('to_date')
     return question("What date do you want to return?")
     
 
 @ask.intent("DateIntent", state='to_date')
 def to_date(date):
-    state.transition = 'confirm'
+    state.transition('confirm')
     return question("Do you want to confirm this trip?")    
 
 
@@ -58,7 +58,7 @@ def confirm_yes():
 
 @ask.intent("AMAZON.NoIntent", state='confirm')
 def confirm_no():
-    state.transition = 'from_city'
+    state.transition('from_city')
     return question("What city are you leaving from?")
 
 
