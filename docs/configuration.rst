32a1735d72450bae860c09adfc982b82fbf66cb5
johnwheeler <john@johnwheeler.org>
2016-08-10 08:49:24 -0700

    closes #20 - Add feature to disable request verification for testing the apps

--- docs/configuration.rst
+++ docs/configuration.rst
@@ -11,6 +11,11 @@ Flask-Ask exposes the following configuration variables:
                              list of allowed application IDs. By default, application ID verification is disabled and a
                              warning is logged. This variable should be set in production to ensure
                              requests are being sent by the applications you specify. **Default:** ``None``
+`ASK_VERIFY_REQUESTS`        Enables or disables 
+                             `Alexa request verification <https://developer.amazon.com/public/solutions/alexa/alexa-skills-kit/docs/developing-an-alexa-skill-as-a-web-service#checking-the-signature-of-the-request>`_, 
+                             which ensures requests sent to your skill are 
+                             from Amazon's Alexa service. This setting should not be disabled in production. It is 
+                             useful for mocking JSON requests in automated tests. **Default:** ``True``
 `ASK_VERIFY_TIMESTAMP_DEBUG` Turn on request timestamp verification while debugging by setting this to ``True``.
                              Timestamp verification helps mitigate against
                              `replay attacks <https://en.wikipedia.org/wiki/Replay_attack>`_. It
--- flask_ask/core.py
+++ flask_ask/core.py
@@ -41,12 +41,17 @@ class Ask(object):
     def init_app(self, app):
         if self._route is None:
             raise TypeError("route is a required argument when app is not None")
+            
         app.ask = self
+        
+        self.ask_verify_requests = app.config.get('ASK_VERIFY_REQUESTS', True)
         self.ask_verify_timestamp_debug = app.config.get('ASK_VERIFY_TIMESTAMP_DEBUG', False)
         self.ask_application_id = app.config.get('ASK_APPLICATION_ID', None)
-        if self.ask_application_id is None:
+        
+        if self.ask_verify_requests and self.ask_application_id is None:
             logger.warning("The ASK_APPLICATION_ID has not been set. Application ID verification disabled.")
-        app.add_url_rule(self._route, view_func=self._flask_view_func, methods=['POST'])
+            
+        app.add_url_rule(self._route, view_func=self._flask_view_func, methods=['POST'])        
         app.jinja_loader = ChoiceLoader([app.jinja_loader, YamlLoader(app)])
 
     def on_session_started(self, f):
@@ -110,27 +115,30 @@ class Ask(object):
     def convert_errors(self, value):
         _app_ctx_stack.top._ask_convert_errors = value
 
-    def _verified_request(self):
+    def _alexa_request(self, verify=True):
         raw_body = flask_request.data
+        alexa_request_payload = json.loads(raw_body)        
         cert_url = flask_request.headers['Signaturecertchainurl']
         signature = flask_request.headers['Signature']
-        # load certificate - this verifies a the certificate url and format under the hood
-        cert = verifier.load_certificate(cert_url)
-        # verify signature
-        verifier.verify_signature(cert, signature, raw_body)
-        # verify timestamp
-        ask_payload = json.loads(raw_body)
-        timestamp = aniso8601.parse_datetime(ask_payload['request']['timestamp'])
-        if not current_app.debug or self.ask_verify_timestamp_debug:
-            verifier.verify_timestamp(timestamp)
-        # verify application id
-        application_id = ask_payload['session']['application']['applicationId']
-        if self.ask_application_id is not None:
-            verifier.verify_application_id(application_id, self.ask_application_id)
-        return ask_payload
+        
+        if verify:
+            # load certificate - this verifies a the certificate url and format under the hood
+            cert = verifier.load_certificate(cert_url)        
+            # verify signature
+            verifier.verify_signature(cert, signature, raw_body)        
+            # verify timestamp
+            timestamp = aniso8601.parse_datetime(alexa_request_payload['request']['timestamp'])
+            if not current_app.debug or self.ask_verify_timestamp_debug:
+                verifier.verify_timestamp(timestamp)            
+            # verify application id
+            application_id = alexa_request_payload['session']['application']['applicationId']
+            if self.ask_application_id is not None:
+                verifier.verify_application_id(application_id, self.ask_application_id)
+            
+        return alexa_request_payload
 
     def _flask_view_func(self, *args, **kwargs):
-        ask_payload = self._verified_request()
+        ask_payload = self._alexa_request(verify=self.ask_verify_requests)
         _dbgdump(ask_payload)
         request_body = _parse_request_body(ask_payload)
         self.request = request_body.request
