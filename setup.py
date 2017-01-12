3303f74da94ea2c615d641207bec4b148064076d
johnwheeler <john@johnwheeler.org>
2017-01-12 11:02:14 -0800

    version bump

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ from setuptools import setup
 
 setup(
     name='Flask-Ask',
-    version='0.8.4',
+    version='0.8.5',
     url='https://github.com/johnwheeler/flask-ask',
     license='Apache 2.0',
     author='John Wheeler',
