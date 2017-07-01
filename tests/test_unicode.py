4bf3ddf18753a94f06bf7f6aec2865e3aff3f610
Dave Voutila <voutilad@gmail.com>
2017-07-01 11:18:15 -0400

    better test for issue #147 just to make sure the text is rendered.

--- tests/test_unicode.py
+++ tests/test_unicode.py
@@ -6,12 +6,17 @@ from flask_ask import statement, question
 class UnicodeTests(unittest.TestCase):
     """ Test using Unicode in responses. (Issue #147) """
 
+    unicode_string = u"Was kann ich für dich tun?"
+
     def test_unicode_statements(self):
         """ Test unicode statement responses """
-        stmt = statement(u"Was kann ich für dich tun?")
-        self.assertIsNotNone(stmt)
+        stmt = statement(self.unicode_string)
+        speech = stmt._response['outputSpeech']['text']
+        print(speech)
+        self.assertTrue(self.unicode_string in speech)
 
     def test_unicode_questions(self):
         """ Test unicode in question responses """
-        q = question(u"Was kann ich für dich tun?")
-        self.assertIsNotNone(q)
+        q = question(self.unicode_string)
+        speech = q._response['outputSpeech']['text']
+        self.assertTrue(self.unicode_string in speech)
