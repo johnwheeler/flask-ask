978881f3ba1a452e543eac9f7f566ed1499f5ad3
Ian Young <ian@duffrecords.com>
2018-09-10 13:43:41 -0700

    integration tests for Gadgets Skill API

--- tests/test_gadget.py
+++ tests/test_gadget.py
@@ -2,7 +2,8 @@ import unittest
 import json
 import uuid
 
-from flask_ask import Ask, gadget, animation, animation_step
+from datetime import datetime
+from flask_ask import Ask, gadget, animation, animation_step, question, session
 from flask import Flask
 from mock import patch, MagicMock
 
@@ -133,6 +134,143 @@ class GadgetUnitTests(unittest.TestCase):
         }
         self.assertEqual(directive['recognizers'], recognizers)
         self.assertEqual(directive['events'], events)
+
+
+basic_request = {
+    "version": "1.0",
+    "session": {
+        "new": True,
+        "sessionId": "amzn1.echo-api.session.0000000-0000-0000-0000-00000000000",
+        "application": {
+            "applicationId": "fake-application-id"
+        },
+        "attributes": {},
+        "user": {
+            "userId": "amzn1.account.AM3B00000000000000000000000"
+        }
+    },
+    "context": {
+        "System": {
+            "application": {
+                "applicationId": "fake-application-id"
+            },
+            "user": {
+                "userId": "amzn1.account.AM3B00000000000000000000000"
+            },
+            "device": {
+                "supportedInterfaces": {
+                    "GadgetController": {},
+                    "GameEngine": {}
+                }
+            }
+        }
+    },
+    "request": {}
+}
+
+
+class GadgetIntegrationTests(unittest.TestCase):
+
+    def setUp(self):
+        self.app = Flask(__name__)
+        self.app.config['ASK_VERIFY_REQUESTS'] = False
+        self.ask = Ask(app=self.app, route='/ask')
+        self.client = self.app.test_client()
+
+        @self.ask.intent('RollCallIntent')
+        def start_roll_call():
+            speech = 'Players, press your buttons now.'
+            return gadget(speech).roll_call(timeout=10000, max_buttons=2)
+
+        @self.ask.on_input_handler_event()
+        def event_received(type, requestId, originatingRequestId, events):
+            for event in events:
+                for input_event in event['inputEvents']:
+                    if session.attributes['activity'] == 'roll call':
+                        return register_player(event['name'], input_event)
+                    elif session.attributes['activity'] == 'new round':
+                        return buzz_in(input_event)
+
+        def register_player(event_name, input_event):
+            """Adds a player's button to the list of known buttons and makes the button pulse yellow."""
+            if input_event['action'] == 'down':
+                button_number = event_name[-1]
+                gid = input_event['gadgetId']
+                session.attributes['players'].append({'pid': button_number, 'gid': gid})
+                speech = ""
+                if event_name.endswith('2'):
+                    session.attributes['activity'] = 'roll call complete'
+                    speech = 'I found {} buttons. Ready to start the round?'.format(2)
+                return gadget(speech).set_light(
+                    targets=[gid],
+                    animations=[animation().pulse(color='FFFF00', duration=100)]
+                )
+
+        def buzz_in(input_event):
+            """Acknowledges the first button that was pressed with speech and a 'breathing' animation."""
+            gid = input_event['gadgetId']
+            try:
+                pid = [p['pid'] for p in session.attributes['players'] if p['gid'] == gid][0]
+            except LookupError:
+                return question("I couldn't find the player associated with that button.")
+            return gadget("Player {}, you buzzed in first.".format(pid)).set_light(
+                targets=[gid],
+                animations=[animation(repeat=3).breathe(duration=500, color='00FF00')]
+            )
+
+    def tearDown(self):
+        pass
+
+    def test_response_to_roll_call(self):
+        for button in range(1, 3):
+            event = {
+                'type': 'GameEngine.InputHandlerEvent',
+                'requestId': 'amzn1.echo-api.request.{}'.format(str(uuid.uuid4())),
+                'events': [{
+                    'name': 'roll_call_event_btn{}'.format(button),
+                    'inputEvents': [{
+                        'gadgetId': 'amzn1.ask.gadget.{}'.format(button),
+                        'timestamp': datetime.now().isoformat(),
+                        'color': '000000',
+                        'feature': 'press',
+                        'action': 'down'
+                    }]
+                }]
+            }
+            basic_request['request'] = event
+            basic_request['session']['attributes']['activity'] = 'roll call'
+            basic_request['session']['attributes']['players'] = []
+            response = self.client.post('/ask', data=json.dumps(basic_request))
+            self.assertEqual(200, response.status_code)
+            response_data = json.loads(response.data.decode('utf-8'))
+            self.assertEqual(response_data['sessionAttributes']['players'][0]['gid'], 'amzn1.ask.gadget.{}'.format(button))
+            self.assertEqual(response_data['sessionAttributes']['players'][0]['pid'], str(button))
+
+    def test_first_button(self):
+        event = {
+            'type': 'GameEngine.InputHandlerEvent',
+            'requestId': 'amzn1.echo-api.request.{}'.format(str(uuid.uuid4())),
+            'events': [{
+                'name': 'button_down_event',
+                'inputEvents': [{
+                    'gadgetId': 'amzn1.ask.gadget.1',
+                    'timestamp': datetime.now().isoformat(),
+                    'color': '000000',
+                    'feature': 'press',
+                    'action': 'down'
+                }]
+            }]
+        }
+        basic_request['request'] = event
+        basic_request['session']['attributes']['activity'] = 'new round'
+        basic_request['session']['attributes']['players'] = [
+            {"gid": "amzn1.ask.gadget.1", "pid": "1"},
+            {"gid": "amzn1.ask.gadget.2", "pid": "2"}
+        ]
+        response = self.client.post('/ask', data=json.dumps(basic_request))
+        self.assertEqual(200, response.status_code)
+        response_data = json.loads(response.data.decode('utf-8'))
+        self.assertEqual(response_data['response']['outputSpeech']['text'], 'Player 1, you buzzed in first.')
 
 
 if __name__ == '__main__':
